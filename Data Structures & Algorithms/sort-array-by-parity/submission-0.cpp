a76453a69d458610d6c1b7fd4aa16c3fd5e1f5e6
Janhvi Shukla <136268489+Janhvi-S103@users.noreply.github.com>
2026-07-10 01:08:12 +0530

    Add: sort-array-by-parity - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int j=nums.size()-1;
+        int i=0;
+        while(i <=j)
+        {
+            if(nums[i]%2 != 0 && nums[j]%2 ==0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            if(nums[i]%2 == 0)
+            {
+                i++;
+            }
+            if(nums[j]%2 !=0)
+            {
+                j--;
+            }
+        }
+        return nums;
+    }
+};
