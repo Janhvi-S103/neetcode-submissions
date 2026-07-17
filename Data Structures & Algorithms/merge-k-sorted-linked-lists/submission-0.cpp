df5f25b5a5a31dd48b9090e1af0d88e3fbb01440
Janhvi Shukla <136268489+Janhvi-S103@users.noreply.github.com>
2026-07-17 19:04:34 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty())
+            return nullptr;
+        ListNode* res= lists[0];
+        for(int i=1; i < lists.size(); i++)
+        {
+            res= mergeTwoLinkedList(res,lists[i]);
+        }
+        return res;
+    }
+
+    ListNode* mergeTwoLinkedList(ListNode* l1, ListNode* l2)
+    {
+        ListNode dummy(0);
+        ListNode *head= &dummy;
+        if(l1 == NULL )
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        if(l1 == NULL && l2 == NULL)
+            return nullptr;
+        while(l1!= NULL && l2 != NULL)
+        {
+            if(l1->val <= l2->val)
+            {
+                head->next=l1;
+                l1=l1->next;
+                head=head->next;
+            }
+            else
+            {
+                head->next = l2;
+                l2=l2->next;
+                head=head->next;
+            }
+        }
+        while(l1!= NULL)
+        {
+            head->next = l1;
+            l1=l1->next;
+            head=head->next;
+        }
+
+        while(l2!= NULL)
+        {
+            head->next = l2;
+            l2=l2->next;
+            head=head->next;
+        }
+        return dummy.next;
+    }
+};
