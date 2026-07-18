957d3be108c665c498299c3834455fcd3d3087ef
Janhvi Shukla <136268489+Janhvi-S103@users.noreply.github.com>
2026-07-19 03:29:19 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l=0, r=0, maxL=0;
+        while(r<s.length())
+        {
+            string sub= s.substr(l,r-l+1);
+            if(isValidSubstr(sub,k))
+            {
+                maxL=max(maxL,isValidSubstr(sub,k));
+                r++;
+            }
+            else
+                l++;
+        }
+        return maxL;
+    }
+
+    int isValidSubstr(string sub, int k)
+    {
+        unordered_map<char, int> freq;
+        int maxr=0;
+        for(auto s: sub)
+        {
+            freq[s]++;
+            maxr = max(maxr, freq[s]);
+        }
+
+        if(sub.length()- maxr > k)
+        {
+            return 0;
+        }
+        else
+            return sub.length();
+    }
+};
